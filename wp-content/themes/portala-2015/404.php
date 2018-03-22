371b8bd36c738d1675c0268114fd8986bbcbd805
Kirk Pettinga <kirk.pettinga@gmail.com>
2018-03-22 10:58:16 +0100

    remove old portal-a theme

--- wp-content/themes/portala-2015/404.php
+++
@@ -1,6 +0,0 @@
-<?php get_header(); ?>
-
-	<h2>Page Not Found</h2>
-	<p>Please check out our <a href="<?= get_permalink(2) ?>">home</a> or <a href="<?= get_permalink(12) ?>">work</a> page.</p>
-	
-<?php get_footer(); ?>
--- wp-content/themes/portala-2015/archive.php
+++
@@ -1,5 +0,0 @@
-<?php get_header(); ?>
-
-<?php include('posts.php'); ?>
-
-<?php get_footer(); ?>
