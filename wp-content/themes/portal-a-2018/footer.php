f48c36d64c190124bf055d41f44161d555aa54ec
Kirk Pettinga <kirk.pettinga@gmail.com>
2018-04-17 15:51:26 +0200

    new footer updates add icons

--- wp-content/themes/portal-a-2018/footer.php
+++ wp-content/themes/portal-a-2018/footer.php
@@ -1,22 +1,40 @@
 </main>
 <!-- .content -->
 
-<footer class="mainfoot">
-	<div class="pa-l-px-1">
+<footer class="mainfoot pa-l-pa-1">
+	
+	<p class="pa-l-mt-0" style="letter-spacing: 0.0526em">
 		Portal A Limited <?php echo date('Y') ?> //
 		<?php echo get_post_meta(2, 'locations', TRUE) ?> //
 		<a href="mailto:<?php echo get_post_meta(2, 'info_email', TRUE) ?>">
 			<?php echo get_post_meta(2, 'info_email', TRUE) ?>
 		</a>
-		<span class="social">
-			<a href="<?php echo get_post_meta(2, 'facebook_url', TRUE) ?>" target="_blank" class="facebook">Facebook</a>
-			<a href="<?php echo get_post_meta(2, 'twitter_url', TRUE) ?>" target="_blank" class="twitter">Twitter</a>
-			<a href="<?php echo get_post_meta(2, 'linkedin_url', TRUE) ?>" target="_blank" class="linkedin">LinkedIn</a>
-			<a href="<?php echo get_post_meta(2, 'instagram_url', TRUE) ?>" target="_blank" class="instagram">Instagram</a>
-			<a href="<?php echo get_post_meta(2, 'youtube_url', TRUE) ?>" target="_blank" class="youtube">YouTube</a>
-			<a href="<?php echo get_permalink(87) ?>" class="blog-link">Portal A Blog</a>
-		</span>
-	</div>
+	</p>
+
+	<span class="social">
+		<a href="<?php echo get_post_meta(2, 'youtube_url', TRUE) ?>" target="_blank" class="pa-l-ml-1">
+			<span class="pa-u-hide">YouTube</span>
+			<i class="pa-b-icon icon-youtube" aria-hidden="true"></i>
+		</a>
+		<a href="<?php echo get_post_meta(2, 'instagram_url', TRUE) ?>" target="_blank" class="pa-l-ml-1">
+			<span class="pa-u-hide">Instagram</span>
+			<i class="pa-b-icon icon-instagram" aria-hidden="true"></i>
+		</a>
+		<a href="<?php echo get_post_meta(2, 'facebook_url', TRUE) ?>" target="_blank" class="pa-l-ml-1">
+			<span class="pa-u-hide">Facebook</span>
+			<i class="pa-b-icon icon-facebook" aria-hidden="true"></i>
+		</a>
+		<a href="<?php echo get_post_meta(2, 'twitter_url', TRUE) ?>" target="_blank" class="pa-l-ml-1">
+			<span class="pa-u-hide">Twitter</span>
+			<i class="pa-b-icon icon-twitter" aria-hidden="true"></i>
+		</a>
+		<a href="<?php echo get_post_meta(2, 'linkedin_url', TRUE) ?>" target="_blank" class="pa-l-ml-1">
+			<span class="pa-u-hide">LinkedIn</span>
+			<i class="pa-b-icon icon-linkedin" aria-hidden="true"></i>
+		</a>
+		<a href="<?php echo get_permalink(87) ?>" class="blog-link pa-l-ml-1">Portal A Blog</a>
+	</span>
+
 </footer>
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
--- wp-content/themes/portal-a-2018/page-home.php
+++ wp-content/themes/portal-a-2018/page-home.php
@@ -14,7 +14,7 @@ $bgVideo = get_field('background_video');
 ?>
 
 <div class="pa-c-hero is-video">
-	<video class="pa-c-hero__media" muted>
+	<video class="pa-c-hero__media" muted autoplay loop>
 		<source src="<?php echo $fileWEBM['url']; ?>" type="video/webm">
 		<source src="<?php echo $fileMP4['url']; ?>" type="video/mp4">
 		<source src="<?php echo $fileOGV['url']; ?>" type="video/ogv"> Your browser does not support the video tag.
@@ -29,12 +29,12 @@ $bgVideo = get_field('background_video');
 
 <div class="pa-l-flexbox pa-l-flex-wrap">
 	
-	<div class="pa-l-flex pa-l-span-6-md pa-l-span-4-lg" style="background-image:url(//unsplash.it/800/500);background-size:cover">
+	<div class="pa-l-flex pa-l-span-6-md pa-l-span-4-lg" style="background-image:url(//unsplash.it/800/500);background-size:cover;background-position:center">
 		<img src="//unsplash.it/800/500" alt="" width="800" height="500" class="pa-u-hide-md"/>
 	</div>
 	<div class="pa-l-flex pa-l-span-6-md pa-l-span-3-lg pa-l-pa-2 pa-u-bg-primary pa-u-color-white">
 		<blockquote class="pa-h3 pa-u-scale-hover pa-u-transition-fast" style="text-transform:none">
-			&ldquo;<br/>
+			<i class="pa-b-icon icon-left-quote" aria-hidden="true" style="font-size:1.25rem"></i><br/>
 			Portal A, more than any other creative shop, has cracked the code of online video.<br/>
 			<br/>
 			<cite>
@@ -42,24 +42,24 @@ $bgVideo = get_field('background_video');
 			</cite>
 		</blockquote>
 	</div>
-	<div class="pa-l-flex pa-l-span-6-md pa-l-span-5-lg" style="background-image:url(//unsplash.it/800/550);background-size:cover">
+	<div class="pa-l-flex pa-l-span-6-md pa-l-span-5-lg" style="background-image:url(//unsplash.it/800/550);background-size:cover;background-position:center">
 		<img src="//unsplash.it/800/550" alt="" width="800" height="550" class="pa-u-hide-md"/>
 	</div>
 	<div class="pa-l-flex pa-l-flexbox pa-l-align-center pa-l-justify-center pa-l-span-6-md pa-l-span-3-lg pa-l-pa-2 pa-u-bg-primary pa-u-color-white">
 		<a href="#" class="pa-u-display-block pa-u-text-center pa-u-scale-hover pa-u-transition-fast pa-u-opaque">
-			<img src="//placehold.it/75x65/ffffff/dddddd" alt="hand icon" width="75" height="65" /><br/>
+			<i class="pa-b-icon icon-hand-wave" aria-hidden="true" style="font-size:4rem"></i><br/>
 			<br/>
 			<p class="pa-h3">NOW HIRING!</p>
 		</a>
 	</div>
-	<div class="pa-l-flex pa-l-span-6-md pa-l-span-4-lg" style="background-image:url(//unsplash.it/800/600);background-size:cover">
+	<div class="pa-l-flex pa-l-span-6-md pa-l-span-4-lg" style="background-image:url(//unsplash.it/800/600);background-size:cover;background-position:center">
 		<img src="//unsplash.it/800/600" alt="" width="800" height="600" class="pa-u-hide-md"/>
 	</div>
 	<div class="pa-l-flex pa-l-span-6-md pa-l-span-5-lg pa-l-pa-2 pa-u-bg-primary pa-u-color-white">
 		<a href="#" class="pa-u-display-block pa-u-scale-hover pa-u-transition-fast pa-u-opaque">
 			<blockquote>
 				<span class="pa-h3" style="text-transform:none">
-					&ldquo;<br/>
+					<i class="pa-b-icon icon-left-quote" aria-hidden="true" style="font-size:1.25rem"></i><br/>
 					We’re thrilled to welcome six bright and talented new members to the Portal A family, and congratulate our all-stars on their recent promotions.<br/>
 				</span>
 				<br/>
