b9e38770a0ae79e9e40c079074d206e6a26a12bc
Kirk Pettinga <kirk.pettinga@gmail.com>
2018-06-05 13:38:29 +0200

    rework tile fields convert home page to blocks

---
+++ wp-content/themes/portal-a-2018/front-page.php
@@ -0,0 +1,45 @@
+<?php 
+
+/**
+ * Front Page
+ */
+
+get_header(); ?>
+
+<?php
+$fileMP4 = get_field('mp4_video_file');
+$fileOGV = get_field('ogv_video_file');
+$fileWEBM = get_field('webm_video_file');
+?>
+
+<div class="pa-c-hero is-video">
+	<div class="pa-c-hero__media pa-c-cover-media is-fullscreen">
+		<video muted autoplay loop>
+			<source src="<?php echo $fileWEBM['url']; ?>" type="video/webm">
+			<source src="<?php echo $fileMP4['url']; ?>" type="video/mp4">
+			<source src="<?php echo $fileOGV['url']; ?>" type="video/ogv"> Your browser does not support the video tag.
+		</video>
+	</div>
+	<div class="pa-c-hero__content">
+		
+		<h1 class="pa-c-hero__title">Breakthrough entertainment<br/>for a new generation</h1>
+
+		<div class="pa-l-flexbox pa-l-justify-space-between" style="width:100%">
+			<a href="#blocks" class="js-smooth-scroll pa-u-scale-hover pa-u-transition-fast">
+				<span class="pa-u-hide">Scroll down</span>
+				<span class="pa-b-icon icon-arrow-down" aria-hidden="true" style="font-size:2.25rem"></span>
+			</a>
+			<a href="#" class="pa-u-scale-hover pa-u-transition-fast">
+				<span class="pa-u-hide">Unmute video</span>
+				<span class="pa-b-icon icon-sound" aria-hidden="true" style="font-size:2.25rem"></span>
+			</a>
+		</div>
+		
+	</div>
+</div>
+
+<div id="blocks">
+	<?php get_template_part( 'components/blocks' ) ?>
+</div>
+
+<?php get_footer(); ?>
--- wp-content/themes/portal-a-2018/includes/component-functions.php
+++ wp-content/themes/portal-a-2018/includes/component-functions.php
@@ -4,6 +4,28 @@
  * ------------------------------------------------------- */
 
 /**
+ * banner
+ * ------------------------------------------------------- */
+function pa_block_banner( $data, $options = array(), $return = false ) {
+
+    ob_start(); 
+    ?>
+
+        <div class="pa-c-block--banner">
+            <h2 class="pa-u-uppercase pa-u-weight-bold pa-l-ma-0 pa-l-py-1 pa-l-px-gutter pa-u-text-center"><?php echo $data['text'] ?></h2>
+		</div>
+
+    <?php
+    $html = ob_get_clean();
+    
+    if ( $return ) :
+        return $html;
+    else :
+        echo $html;
+    endif;
+}
+
+/**
  * blockquote
  * ------------------------------------------------------- */
 function pa_block_blockquote( $data, $options = array(), $return = false ) {
@@ -283,51 +305,103 @@ function pa_block_tiles( $data, $options = array(), $return = false ) {
 
     $options = array_merge( $defaults, $options );
 
+    $tile_options = $data['tile_options'] ? $data['tile_options'] : array();
+    $has_container = in_array( 'container', $tile_options ) ;
+    $has_spacing = in_array( 'spacing', $tile_options );
+
     ob_start(); 
     ?>
 
-        <div class="pa-c-block--tiles" style="<?php echo $options['style'] ?>">
+        <div class="pa-c-block--tiles <?php echo $has_container ? 'pa-l-container' : '' ?>" style="<?php echo $options['style'] ?>">
 
-            <div class="pa-l-container">
-            
-                <div class="pa-l-flexbox pa-l-flex-wrap pa-l-with-gutters">
+            <div class="pa-l-flexbox pa-l-flex-wrap <?php echo $has_spacing ? 'pa-l-with-gutters' : '' ?>">
 
-                    <?php foreach ( $data['tiles'] as $tile ) :
-                        
-                        $span = $tile['width'] === 'third' ? 'pa-l-span-4-md' : 'pa-l-span-12';
-                        $url = $tile['url'];
-                        $tag = $url ? 'a' : 'div';
-                        $href = $url ? "href=\"$url\"" : ""; ?>
+                <?php foreach ( $data['tiles'] as $tile ) :
+                    
+                    switch( $tile['columns'] ) :
+                        case '3':
+                            $span = 'pa-l-span-6-md pa-l-span-3-lg';
+                            break;
+                        case 'third':
+                        case '4':
+                            $span = 'pa-l-span-6-md pa-l-span-4-lg';
+                            break;
+                        case '5':
+                            $span = 'pa-l-span-6-md pa-l-span-5-lg';
+                            break;
+                        case 'half':
+                        case '6':
+                            $span = 'pa-l-span-6-md';
+                            break;
+                        case '7':
+                            $span = 'pa-l-span-12-md pa-l-span-7-lg';
+                            break;
+                        case '8':
+                            $span = 'pa-l-span-12-md pa-l-span-8-lg';
+                            break;
+                        case '9':
+                            $span = 'pa-l-span-12-md pa-l-span-9-lg';
+                            break;
+                        default :
+                            $span = 'pa-l-span-12';
+                    endswitch;
+                    
+                    $span = 'pa-l-flex ' . $span;
+                    $url = $tile['url'];
+                    $tag = $url ? 'a' : 'div';
+                    $href = $url ? "href=\"$url\"" : "";
+                    $gutter = $has_spacing ? 'pa-l-mb-gutter' : ''; ?>
 
-                        <?php if ( $tile['type'] === 'image' ) :
-                        
-                            $padding = $tile['width'] === 'third' ? 'padding-top: 100%' : 'padding-top: 33.333%' ?>
 
-                            <?php echo "<$tag $href class=\"pa-l-flex $span pa-l-mb-gutter\">" ?>
-                                <div class="pa-c-cover-media" style="<?php echo $padding ?>">
-                                    <?php echo wp_get_attachment_image( $tile['image'], 'large' ); ?>
+                    <?php if ( $tile['type'] === 'image' ) :
+
+    
+                        $options = $tile['image_options'] ? $tile['image_options'] : array();
+                        $image_meta = wp_get_attachment_metadata( $tile['image'] );
+                        $ratio = ($image_meta['height'] / $image_meta['width']) * 100;
+                        $proportional_image = in_array( 'proportional_image', $options );
+                        $padding = $proportional_image ? $ratio / ( 12 / $tile['columns'] ) : 0;
+                        $effects = $url ? 'does-scale does-fade' : ''; ?>
+
+                        <?php echo "<$tag $href class=\"pa-c-cover-media $span $gutter $effects\" style=\"padding-top:{$padding}%\">"?>
+                            <?php echo wp_get_attachment_image( $tile['image'], 'large' ); ?>
+                            <?php if ( $tile['text'] ) : ?>
+                                <div class="pa-c-cover-media__content is-animated fade-in from-bottom use-light-ui pa-u-text-center" style="width:100%">
+                                    <p class="pa-h4 pa-l-mt-0"><?php echo $tile['text'] ?></p>
                                 </div>
-                            <?php echo "</$tag>" ?>
+                            <?php endif; ?>
+                        <?php echo "</$tag>" ?>
 
-                        <?php else : ?>
 
-                            <?php echo "<$tag $href class=\"pa-l-flex $span pa-l-flexbox pa-l-flex-wrap pa-l-justify-space-between pa-u-text-center pa-l-pa-1 pa-l-mb-gutter pa-u-bg-primary pa-u-color-white\">" ?>
-                                <div class="pa-u-center pa-l-mt-nudge" style="width:100%">
-                                    <span class="pa-b-icon icon-left-quote" aria-hidden="true"></span>
-                                    <p class="pa-u-weight-bold pa-l-px-1">
-                                        <?php echo $tile['text'] ?>
-                                    </p>
-                                </div>
-                                <div class="pa-h3 pa-u-center pa-l-align-self-end" style="font-weight:300">
+                    <?php else :
+                     
+                     
+                        $options = $tile['options'] ? $tile['options'] : array();
+                        $effects = $url ? 'pa-u-scale-hover pa-u-transition-slow' : '';
+                        $is_centered = in_array( 'center', $options );
+                        $text_align = $is_centered ? 'pa-u-text-center' : '';
+                        $padding = $is_centered ? 'pa-l-py-3 pa-l-px-1' : 'pa-l-pa-3';
+                        $icon_size = in_array( 'large_icon', $options ) ? 'font-size:4rem' : ''; ?>
+
+                        <?php echo "<div class=\"$span $gutter $padding pa-l-flexbox pa-l-flex-wrap pa-l-align-center pa-l-justify-space-between pa-u-bg-primary pa-u-color-white\">" ?>
+                            <?php echo "<$tag $href class=\"pa-u-display-block $effects $text_align\" style=\"width:100%\">"; ?>
+                                <p class="pa-l-mt-0">
+                                    <?php echo "<span class=\"pa-b-icon icon-{$tile['icon']}\" aria-hidden=\"true\" style=\"$icon_size\"></span>" ?>
+                                </p>
+                                <p style="max-width:450px">
+                                    <span class="pa-u-weight-bold"><?php echo $tile['text'] ?></span>
+                                </p>
+                                <p class="pa-h3 pa-l-align-self-end pa-u-weight-light">
+                                    <?php echo wp_get_attachment_image( $tile['source_image'], 'thumbnail', false, array( 'style' => 'max-height: 24px;width:auto;height:auto' ) ) ?>
                                     <?php echo $tile['source'] ?>
-                                </div>
+                                </p>
                             <?php echo "</$tag>" ?>
+                        </div>
 
-                        <?php endif; ?>
 
-                    <?php endforeach; ?>
+                    <?php endif; ?>
 
-                </div>
+                <?php endforeach; ?>
 
             </div>
 
