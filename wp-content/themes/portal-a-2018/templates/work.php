b78b35901d8b83815b2800d8770ff4b19b158bf5
Kirk Pettinga <kirk.pettinga@gmail.com>
2018-06-13 15:30:15 +0200

    work: use hero component #43

--- wp-content/themes/portal-a-2018/templates/work.php
+++ wp-content/themes/portal-a-2018/templates/work.php
@@ -5,18 +5,6 @@ TEMPLATE NAME: Work
 
 get_header();
 
-$hero = new WP_Query( array(
-	'post_type' => 'work',
-	'posts_per_page' => 1,
-	'tax_query' => array(
-		array(
-			'taxonomy' => 'work-featured',
-			'field' => 'slug',
-			'terms' => array( 'work-page-hero' )
-		)
-	)
-) );
-
 $branded = new WP_Query( array(
 	'post_type' => 'work',
 	'posts_per_page' => 500,
@@ -51,39 +39,39 @@ $originals = new WP_Query( array(
 	)
 ) ); ?>
 
+<?php while ( have_posts() ) : the_post(); ?>
 
-<!-- page title for SEO, screen readers -->
-<h1 style="display:none"><?php the_title(); ?></h1>
-<!-- // -->
-
-
-<?php while ( $hero->have_posts() ) : $hero->the_post(); ?>
-
-	<div <?php post_class("pa-c-hero is-left-aligned") ?> >
-		
+	<div class="pa-c-hero">
+			
 		<div class="pa-c-hero__media pa-c-cover-media">
 			<?php the_post_thumbnail( 'hero' ); ?>
 		</div>
 
-		<div class="pa-c-hero__content">
-			<a href="<?php the_permalink() ?>" class="pa-u-scale-hover pa-u-transition-slow pa-u-display-block" title="<?php the_title(); ?>">
-				<div class="pa-c-hero__title">
-					<p>
-						<?php 
-						if ( $client_image_id = get_post_meta( get_the_ID(), 'client_image', true ) ) {
-							echo wp_get_attachment_image( $client_image_id, 'full', false, array( 'style' => 'width:auto;height:auto;max-width:70px;max-height:70px' ) );
-						} else {
-							echo get_post_meta( get_the_ID(), 'client', true );
-						} ?>
-					</p>
-					<h2 class="pa-work-title"><?php echo $post->post_excerpt ? $post->post_excerpt : get_the_title(); ?></h2>
-				</div>
-			</a>
+		<div class="pa-c-hero__tab">
+			<h1 class="pa-u-text-center pa-l-mt-0">
+				<?php 
+				if ( $post->post_excerpt ) :
+					echo $post->post_excerpt;
+				else :
+					the_title();
+				endif; 
+				?>
+			</h1>
 		</div>
-
+		
 	</div>
+
 <?php endwhile; ?>
 
+<div class="pa-l-container">
+		
+	<?php pa_block_wysiwyg( array( 
+		'wysiwyg' => apply_filters( 'the_content', get_the_content() ),
+	) ) ?>
+	
+</div>
+
+<?php get_template_part( 'partials/blocks' ) ?>
 
 <nav id="view-nav" class="pa-l-ma-0 pa-l-py-1 pa-u-text-center" aria-label="Work Type Navigation">
 	<a href="#branded" class="pa-b-view-toggle js-view-toggle is-active">Branded</a>
