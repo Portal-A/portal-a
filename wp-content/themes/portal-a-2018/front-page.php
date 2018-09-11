8f137083562ac0c6c19c2189ef59da0d20f8c084
Kirk Pettinga <kirk.pettinga@gmail.com>
2018-09-11 18:13:48 +0200

    home tweaks

--- wp-content/themes/portal-a-2018/front-page.php
+++ wp-content/themes/portal-a-2018/front-page.php
@@ -24,23 +24,25 @@ get_header(); ?>
 		</div>
 		<div class="pa-c-home-video__content">
 			
-			<h1 class="pa-c-home-video__title"><?php echo $post->post_excerpt ? $post->post_excerpt : get_the_title(); ?></h1>
-
 			<div class="pa-l-flexbox pa-l-justify-space-between" style="width:100%">
+			
+				<h1 class="pa-c-home-video__title"><?php echo $post->post_excerpt ? $post->post_excerpt : get_the_title(); ?></h1>
+
 				<button href="#reel" 
 						class="pa-b-clean-button pa-u-scale-hover pa-u-transition-fast js-lightbox-toggle"
 						style="margin-left:auto"
 						data-template="#full-reel"
 						data-template-data='{ "source": "<?php echo $fileBackground ?>" }'>
 					<span class="pa-u-hide">Open video lightbox</span>
-					<span class="pa-b-icon icon-sound" aria-hidden="true" style="font-size:2.25rem"></span>
+					<span class="pa-b-icon icon-sound" aria-hidden="true" style="font-size:2rem"></span>
 				</button>
+
 			</div>
 			
 		</div>
 	</div>
 
-	<div style="position:relative">
+	<div style="position:relative; background:black">
 
 		<?php if ( is_front_page() ) { ?>
 			<nav class="pa-c-home-nav">
