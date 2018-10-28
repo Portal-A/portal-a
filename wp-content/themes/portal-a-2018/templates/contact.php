245e106cfcf016724a68e606825580451b44efce
Kirk Pettinga <kirk.pettinga@gmail.com>
2018-10-28 13:30:28 +0100

    add office page icons #65

--- wp-content/themes/portal-a-2018/templates/contact.php
+++ wp-content/themes/portal-a-2018/templates/contact.php
@@ -20,43 +20,43 @@ get_header();
                     'wysiwyg' => apply_filters( 'the_content', get_the_content() ),
                 ) ) ?>
                 
-                <div class="pa-l-flexbox does-wrap with-gutters pa-l-mt-5">
+                <div class="pa-l-flexbox does-wrap with-gutters pa-l-mt-5 pa-l-mb-4">
         
                     <div class="pa-l-flex span-6-md span-4-lg pa-u-text-center pa-l-mb-4">
-                        [icon]<br/>
-                        <p class="pa-l-mt-half">
+                        <i class="pa-b-icon icon-envelope pa-u-display-inline-block" style="min-height: 72px; font-size:60px" aria-hidden="true"></i>
+                        <p class="pa-h2 pa-l-mt-half">
                             <strong>New Projects</strong><br/>
                             <a href="mailto:biz@portal-a.com" class="pa-u-color-hover-primary">biz@portal-a.com</a>
                         </p>
                     </div>
                     
                     <div class="pa-l-flex span-6-md span-4-lg pa-u-text-center pa-l-mb-4">
-                        [icon]<br/>
-                        <p class="pa-l-mt-half">
+                        <i class="pa-b-icon icon-team pa-u-display-inline-block" style="min-height: 72px; font-size:60px" aria-hidden="true"></i>
+                        <p class="pa-h2 pa-l-mt-half">
                             <strong>Join Our Team</strong><br/>
                             <a href="mailto:jobs@portal-a.com" class="pa-u-color-hover-primary">jobs@portal-a.com</a>
                         </p>
                     </div>
                     
                     <div class="pa-l-flex span-6-md span-4-lg pa-u-text-center pa-l-mb-4">
-                        [icon]<br/>
-                        <p class="pa-l-mt-half">
+                        <i class="pa-b-icon icon-hand pa-u-display-inline-block" style="min-height: 72px; font-size:72px" aria-hidden="true"></i>
+                        <p class="pa-h2 pa-l-mt-half">
                             <strong>Say Hello</strong><br/>
                             <a href="mailto:info@portal-a.com" class="pa-u-color-hover-primary">info@portal-a.com</a>
                         </p>
                     </div>
                     
                     <div class="pa-l-flex span-6-md span-4-lg pa-u-text-center pa-l-mb-4">
-                        [icon]<br/>
-                        <p class="pa-l-mt-half">
+                        <i class="pa-b-icon icon-pencil pa-u-display-inline-block" style="min-height: 72px; font-size:60px" aria-hidden="true"></i>
+                        <p class="pa-h2 pa-l-mt-half">
                             <strong>Press Inquiries</strong><br/>
                             <a href="mailto:donna@portal-a.com" class="pa-u-color-hover-primary">donna@portal-a.com</a>
                         </p>
                     </div>
                     
                     <div class="pa-l-flex span-6-md span-4-lg pa-u-text-center pa-l-mb-4">
-                        [icon]<br/>
-                        <p class="pa-l-mt-half">
+                        <i class="pa-b-icon icon-suitcase pa-u-display-inline-block" style="min-height: 72px; font-size:64px" aria-hidden="true"></i>
+                        <p class="pa-h2 pa-l-mt-half">
                             <strong>Representation</strong><br/>
                             <a href="mailto:email@wme.com" class="pa-u-color-hover-primary">email@wme.com</a>
                         </p>
@@ -64,13 +64,14 @@ get_header();
                     
                     <div class="pa-l-flex span-6-md span-4-lg pa-u-text-center pa-l-mb-4">
                         <form >
-                            <div class="pa-u-center" style="position:relative;width:100%;max-width:220px">
+                            <div class="pa-u-center" style="position:relative;width:100%;max-width:255px; margin: 17px 0">
                                 <input type="email" placeholder="Email" name="email" class="pa-b-input pa-u-display-block" style="width:100%" />
                                 <button type="submit" class="pa-b-submit">
-                                    S
+                                    Sign up
                                 </button>
                             </div>
-                            <p class="pa-l-mt-half">
+                            <span class="pa-u-clearfix"></span>
+                            <p class="pa-h2 pa-l-mt-half">
                                 <strong>Stay In The Know</strong><br/>
                                 Rarely sent, often enjoyed
                             </p>
